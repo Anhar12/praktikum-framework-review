704f0c3b0e28bae0f0e797b3cc2501415194f8f7
Anhar12 <anharkhoirun@gmail.com>
2024-10-01 12:13:09 +0800

    Review 4

--- framework/ais/urls.py
+++ framework/ais/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.homepage, name='home'),
     path('about', views.about, name='about'),
+    path('student/', views.student_index, name='student_index'),
+    path('student/create/', views.student_create, name='student_create'),
 ]
